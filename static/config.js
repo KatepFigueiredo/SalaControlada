849c55760bc96e0e05dd1ebedd40bb6fd796efa2
KatepFigueiredo <catarinafigueiredo126@gmail.com>
2025-12-04 18:12:55 +0000

    Tentativa de Fix

--- static/config.js
+++ static/config.js
@@ -2,9 +2,7 @@
 // Configuração automática da URL do backend
 // ==================================================
 
-const BACKEND_URL = window.location.hostname === 'localhost' 
-  ? "http://localhost:5000"           // Desenvolvimento
-  : "https://backend-sala-controlada.onrender.com";       // Produção (Vercel)
+const BACKEND_URL = 'https://backend-sala-controlada.onrender.com';
 
 console.log("Backend URL:", BACKEND_URL);
 console.log("Hostname:", window.location.hostname);
