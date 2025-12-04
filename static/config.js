2cc11a6833fbb2def476c44a5963c2b488c37050
KatepFigueiredo <catarinafigueiredo126@gmail.com>
2025-12-04 19:09:09 +0000

    Atualização FrontEnd

--- static/config.js
+++ static/config.js
@@ -2,7 +2,7 @@
 // Configuração automática da URL do backend
 // ==================================================
 
-const BACKEND_URL = 'https://backend-sala-controlada.onrender.com';
+const BACKEND_URL = 'web-production-e6d1.up.railway.app';
 
 console.log("Backend URL:", BACKEND_URL);
 console.log("Hostname:", window.location.hostname);
